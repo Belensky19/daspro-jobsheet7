556b332cd18e7ec8b942b1229ed348ce7987d359
Belensky19 <prayogakepanjen@gmail.com>
2025-11-06 15:33:37 +0700

    Message

--- SiakadFor16.java
+++ SiakadFor16.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
+
 public class SiakadFor16 {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        int lowest = 0, highest = 100, grade;
+        Scanner sc = new Scanner(System.in);
+
+        double grade, highest = 0, lowest = 100;
+        
+
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Enter number " + i + ": ");
-            grade = input.nextInt();
-            if (grade < lowest) {
-                lowest = grade;
-            }
+            System.out.print("Enter the grade of the " + i + " student: ");
+            grade = sc.nextDouble();
+
             if (grade > highest) {
                 highest = grade;
             }
-        }
-        System.out.println("Lowest number is: " + lowest);
-        System.out.println("Highest number is: " + highest);
+            if (grade < lowest) {
+                lowest = grade;
+            }
 
-input.close();
+           
         }
+
+        System.out.println("\nThe highest grade: " + highest);
+        System.out.println("The lowest grade: " + lowest);
         
+        sc.close();
+    }
 }
